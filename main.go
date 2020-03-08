a8e480bd477d9a59d13a78ff82e8dab9ae8489a9
Ossaija ThankGod <ossaija.thankgod@andela.com>
2020-03-08 09:41:13 +0100

    Wrap calls to cache and db inside a goroutine

--- main.go
+++ main.go
@@ -12,20 +12,23 @@ var rnd = rand.New(rand.NewSource(time.Now().UnixNano()))
 func main() {
 	for i:=0; i < 10; i++ {
 		id := rnd.Intn(10) + 1
-		if b, ok := queryCache(id); ok {
-			fmt.Println("From cache")
-			fmt.Println(b)
-			continue
-		}
-		if b, ok := queryDatabase(id); ok {
-			fmt.Println("From database")
-			fmt.Println(b)
-			continue
-		}
+		go func (id int)  {
+			if b, ok := queryCache(id); ok {
+				fmt.Println("From cache")
+				fmt.Println(b)
+			}
+		}(id)
 
-		fmt.Printf("Book not found with id: '%v'", id)
+		go func (id int)  {
+			if b, ok := queryDatabase(id); ok {
+				fmt.Println("From database")
+				fmt.Println(b)
+			}
+		}(id)
+
 		time.Sleep(150 * time.Millisecond)
 	}
+	time.Sleep(2 * time.Second)
 }
 
 func queryCache(id int) (b Book, ok bool) {
